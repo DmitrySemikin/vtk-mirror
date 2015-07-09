736cf795b0e6eb570e069987690a815390b25f68
Ben Boeckel <ben.boeckel@kitware.com>
2015-07-09 16:39:31 -0400

    vtkTable: fix error message

--- Common/DataModel/vtkTable.cxx
+++ Common/DataModel/vtkTable.cxx
@@ -286,7 +286,7 @@ vtkIdType vtkTable::InsertNextRow(vtkVariantArray* values)
   vtkIdType ncol = this->GetNumberOfColumns();
   if (values->GetNumberOfTuples() != ncol)
     {
-    vtkErrorMacro(<< "Incorrect number of tuples in SetRow");
+    vtkErrorMacro(<< "Incorrect number of tuples in InsertNextRow");
     }
   vtkIdType row = this->InsertNextBlankRow();
   for (vtkIdType i = 0; i < ncol; i++)
