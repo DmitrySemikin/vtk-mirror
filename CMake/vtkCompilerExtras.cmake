8234f7c6519019c00dce0f4f3f327fb6a6e6dfb0
Joachim Pouderoux <joachim.pouderoux@kitware.com>
2015-07-15 16:03:15 +0200

    Add an advanced option to turn ON linker fatal errors

--- CMake/vtkCompilerExtras.cmake
+++ CMake/vtkCompilerExtras.cmake
@@ -14,12 +14,17 @@ if(CMAKE_COMPILER_IS_GNUCXX)
 
   # If we are compiling on Linux then set some extra linker flags too
   if(CMAKE_SYSTEM_NAME MATCHES Linux)
+    option(VTK_LINKER_FATAL_WARNING "Specify if linker warnings must be considered as errors." OFF)
+    mark_as_advanced(VTK_LINKER_FATAL_WARNING)
+    if(VTK_LINKER_FATAL_WARNING)
+      set(CMAKE_EXTRA_SHARED_LINKER_FLAGS "-Wl,--fatal-warnings")
+    endif()
     set(CMAKE_SHARED_LINKER_FLAGS
-      "-Wl,--no-undefined -lc ${CMAKE_SHARED_LINKER_FLAGS}")
+      "${CMAKE_EXTRA_SHARED_LINKER_FLAGS} -Wl,--no-undefined -lc ${CMAKE_SHARED_LINKER_FLAGS}")
     set(CMAKE_MODULE_LINKER_FLAGS
-      "-Wl,--no-undefined -lc ${CMAKE_SHARED_LINKER_FLAGS}")
+      "${CMAKE_EXTRA_SHARED_LINKER_FLAGS} -Wl,--no-undefined -lc ${CMAKE_SHARED_LINKER_FLAGS}")
     set (CMAKE_EXE_LINKER_FLAGS
-      "-Wl,--no-undefined -lc ${CMAKE_SHARED_LINKER_FLAGS}")
+      "${CMAKE_EXTRA_SHARED_LINKER_FLAGS} -Wl,--no-undefined -lc ${CMAKE_SHARED_LINKER_FLAGS}")
   endif()
 
   # Now check if we can use visibility to selectively export symbols
