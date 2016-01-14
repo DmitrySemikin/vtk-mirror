b138a65e78f902f071241f5fd1aed957e9052b7f
Steven Hahn <hahnse@ornl.gov>
2016-01-14 18:07:42 -0500

    Fix -Wnull-conversion warning.

--- Common/Core/vtkSystemIncludes.h
+++ Common/Core/vtkSystemIncludes.h
@@ -116,7 +116,7 @@
 // Otherwise the type is void which is correct for WIN32
 // and SPROC
 #ifdef VTK_USE_PTHREADS
-#define VTK_THREAD_RETURN_VALUE  NULL
+#define VTK_THREAD_RETURN_VALUE  0
 #define VTK_THREAD_RETURN_TYPE   void *
 #endif
 
