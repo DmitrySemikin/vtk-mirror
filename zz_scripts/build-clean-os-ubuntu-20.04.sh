1e55f00f761b0ce9d8de8dd4be213fca6dfa25a9
Dmitrii Semikin <dmitry.semikin@gmail.com>
2020-12-22 23:45:40 +0100

    Working on build script

--- zz_scripts/build-clean-os-ubuntu-20.04.sh
+++ zz_scripts/build-clean-os-ubuntu-20.04.sh
@@ -11,48 +11,85 @@
 
 STARTTIME=$(date +%s)
 
+
+INSTALL_STARTTIME=$(date +%s)
+
 sudo apt-get -y install \
-  cmake \
   git \
   g++ \
+  cmake \
+  ninja-build \
   libgl1-mesa-dev \
   qtbase5-dev \
   pbzip2
 
-if [[ "$?" -ne "0" ]]; then
+RC=$?
+
+INSTALL_ENDTIME=$(date +%s)
+echo "It takes $(($INSTALL_ENDTIME - $INSTALL_STARTTIME)) seconds to complete the installation..."
+
+if [[ $RC != 0 ]]; then
   echo "ERROR: Failed to install packages" >&2
   exit 1;
 fi
 
-# another possible variant: libqt5gui5 - but it seems to provide only runtime
 
 BUILD_STARTTIME=$(date +%s)
 
-
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-ROOT_DIR="${SCRIPT_DIR}/.."
-BUILD_DIR="${ROOT_DIR}/../vtk-build"
-INSTALL_DIR="${ROOT_DIR}/../vtk-install"
+SRC_DIR="${SCRIPT_DIR}/.."
+WORKING_dir="$( cd "${SRC_DIR}/.." >/dev/null 2>&1 && pwd )"
+BUILD_DIR="${WORKING_DIR}/vtk-build"
+INSTALL_DIR="${WORKING_DIR}/vtk-install"
+
+ENV_DESCRIPTION_FILE="${WORKING_DIR}/vtk-build-env.txt"
+echo "GCC --version" > "${ENV_DESCRIPTION_FILE}"
+gcc --version 2>&1 >> "${ENV_DESCRIPTION_FILE}"
+echo "qmake --version" >> "${ENV_DESCRIPTION_FILE}"
+qmake --version 2>&1 >> "${ENV_DESCRIPTION_FILE}"
+echo "cmake --version" >> "${ENV_DESCRIPTION_FILE}"
+cmake --version 2>&1 >> "${ENV_DESCRIPTION_FILE}"
+echo "ninja --version" >> "${ENV_DESCRIPTION_FILE}"
+ninja --version 2>&1 >> "${ENV_DESCRIPTION_FILE}"
+
+BUILD_LOG="${WORKING_DIR}/build.log"
 
 mkdir "${BUILD_DIR}" \
-&& cd "${BUILD_DIR}" \
-&& cmake -DVTK_GROUP_ENABLE_Qt=YES -DCMAKE_INSTALL_PREFIX= "${ROOT_DIR}" \
-&& cmake --build . \
-&& cmake --install .
+&& cmake \
+  -G Ninja \
+  -DCMAKE_VERBOSE_MAKEFILE=ON \
+  -DVTK_GROUP_ENABLE_Qt=YES \
+  -DCMAKE_INSTALL_PREFIX="${INSTALL_DIR}" \
+  -S "${SRC_DIR}" \
+  -B "${BUILD_DIR}"\
+  2>&1 > "${BUILD_LOG}" \
+&& cmake --build "${BUILD_DIR}" \
+  2>&1 >> "${BUILD_LOG}" \
+&& cmake --install "${BUILD_DIR}" \
+  2>&1 > "${BUILD_LOG}"
 
-if [[ "$?" -ne "0" ]]; then
-    echo "ERROR: Failed to build the package" >&2
-    exit 1;
-fi
+RC=$?
 
 BUILD_ENDTIME=$(date +%s)
 echo "It takes $(($BUILD_ENDTIME - $BUILD_STARTTIME)) seconds to complete the build..."
 
+if [[ RC != 0 ]]; then
+    echo "ERROR: Failed to build the package" >&2
+    exit 1;
+fi
 
 
 ARCH_STARTTIME=$(date +%s)
 
-tar --create --file=vtk-binaries.tar.bz2 --use-compress-program=pbzip2 --directory=${BUILD_DIR}/.. ./vtk-build ./vtk-install
+tar \
+  --create \
+  --file=vtk-binaries.tar.bz2 \
+  --use-compress-program=pbzip2 \
+  --directory="${WORKING_dir}" \
+  "${BUILD_DIR}" \
+  "${INSTALL_DIR}" \
+  "${ENV_DESCRIPTION_FILE}" \
+  "${BUILD_LOG}"
 
 ARCH_ENDTIME=$(date +%s)
 echo "It takes $(($ARCH_ENDTIME - $ARCH_STARTTIME)) seconds to complete the arch..."
