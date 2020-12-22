0162464c1c640162df00647ff623f3f14ec00852
Dmitrii Semikin <dmitry.semikin@gmail.com>
2020-12-22 21:00:31 +0100

    update build script to archive the binaries

--- zz_scripts/build-clean-os-ubuntu-20.04.sh
+++ zz_scripts/build-clean-os-ubuntu-20.04.sh
@@ -9,12 +9,15 @@
 # For Actions we don't need to install git (and I guess also cmake).
 # And cloning is done using action
 
+STARTTIME=$(date +%s)
+
 sudo apt-get -y install \
   cmake \
   git \
   g++ \
   libgl1-mesa-dev \
-  qtbase5-dev
+  qtbase5-dev \
+  pbzip2
 
 if [[ "$?" -ne "0" ]]; then
   echo "ERROR: Failed to install packages" >&2
@@ -23,12 +26,36 @@ fi
 
 # another possible variant: libqt5gui5 - but it seems to provide only runtime
 
+BUILD_STARTTIME=$(date +%s)
+
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 ROOT_DIR="${SCRIPT_DIR}/.."
-BUILD_DIR="${ROOT_DIR}/../vtk-mirror-build"
+BUILD_DIR="${ROOT_DIR}/../vtk-build"
+INSTALL_DIR="${ROOT_DIR}/../vtk-install"
 
 mkdir "${BUILD_DIR}" \
 && cd "${BUILD_DIR}" \
-&& cmake -DVTK_GROUP_ENABLE_Qt=YES "${ROOT_DIR}" \
-&& cmake --build .
+&& cmake -DVTK_GROUP_ENABLE_Qt=YES -DCMAKE_INSTALL_PREFIX= "${ROOT_DIR}" \
+&& cmake --build . \
+
+if [[ "$?" -ne "0" ]]; then
+    echo "ERROR: Failed to build the package" >&2
+    exit 1;
+fi
+
+BUILD_ENDTIME=$(date +%s)
+echo "It takes $(($BUILD_ENDTIME - $BUILD_STARTTIME)) seconds to complete the build..."
+
+
+
+ARCH_STARTTIME=$(date +%s)
+
+tar --create --file=vtk-binaries.tar.bz2 --use-compress-program=pbzip2 --directory=${BUILD_DIR}/.. ./vtk-build ./vtk-install
+
+ARCH_ENDTIME=$(date +%s)
+echo "It takes $(($ARCH_ENDTIME - $ARCH_STARTTIME)) seconds to complete the arch..."
+
+
+ENDTIME=$(date +%s)
+echo "It takes $(($ENDTIME - $STARTTIME)) seconds to complete full task..."
